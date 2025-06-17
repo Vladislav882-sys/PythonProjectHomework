78fc4ff1e4d5daf0ac0d47554b378c067bbf53e6
Vladislav882-sys <vladlord2005@gmail.com>
2025-06-17 14:58:05 +0300

    Create test_login_form_validate.py

---
+++ demoqa_home/tests/test_login_form_validate.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.by import By
+from pages.practice_form_page import PracticeFormPage
+
+def test_placeholder_validation(browser):
+
+    practice_form_page = PracticeFormPage(browser)
+    practice_form_page.visit()
+
+    assert practice_form_page.get_placeholder(By.ID, 'firstName') == 'FirstName'
+    assert practice_form_page.get_placeholder(By.ID, 'lastName') == 'LastName'
+    assert practice_form_page.get_placeholder(By.ID, 'userEmail') == 'name@example.com'
+    assert practice_form_page.get_attribute(By.ID, 'userEmail', 'pattern') == '^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$'
+
+    practice_form_page.submit_form()
+    assert 'was-validated' in practice_form_page.get_form_class()
