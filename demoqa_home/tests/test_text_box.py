d5e52113002425ac22f6f3747ef0bf89e051ff0b
Vladislav882-sys <vladlord2005@gmail.com>
2025-06-17 14:54:01 +0300

    Create test_text_box.py

---
+++ demoqa_home/tests/test_text_box.py
@@ -0,0 +1,17 @@
+from selenium.webdriver.common.by import By
+from pages.text_box_page import TextBoxPage
+
+def test_text_box(browser):
+
+    text_box_page = TextBoxPage(browser)
+    text_box_page.visit()
+
+    full_name = "John Doe"
+    current_address = "123 Main St"
+    text_box_page.fill_full_name(full_name)
+    text_box_page.fill_current_address(current_address)
+
+    text_box_page.submit()
+  
+    assert text_box_page.get_output_full_name() == full_name
+    assert text_box_page.get_output_current_address() == current_address
